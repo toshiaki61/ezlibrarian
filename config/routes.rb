dfb1c963949a208eb00d8b51a2f79bd9ee55f0d4
Michael Musenbrock <redeamer@gmx.net>
2013-05-03 09:57:40 +0200

    Respect changes in origin source in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ match '/treasures/edit_device', :to => "treasures#edit_device"
 
 match '/treasures/:id/add_review', :to => "treasures#add_review"
 
+match '/treasures/:project_id', :controller => 'treasures', :action => 'show_statement'
+
 match '/treasures/:id', :controller => 'treasures', :action => 'show_book'
 match '/treasures/:id', :controller => 'treasures', :action => 'show_device'
 match '/treasures', :controller => 'treasures', :action => 'show_holder_change_histories'
